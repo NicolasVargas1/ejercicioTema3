a768cf29be24268724204d576e5eb417d32461f1
NicolasVargas1 <132532819+NicolasVargas1@users.noreply.github.com>
2023-07-21 16:28:35 -0500

    Add files via upload

---
+++ TAREA 1/src/Main.java
@@ -0,0 +1,19 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        suma(1, 2, 3);
+
+        }
+        public static void suma (int a, int b , int c){
+            int resultado = 0;
+            resultado = a + b + c;
+
+            System.out.println(resultado);
+
+        }
+ }
+
+
+
+    }
---
+++ TAREA1.1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args){
+
+        Car myCar = new Car();
+
+        myCar.incrementDoors();
+
+        System.out.println(myCar.numberOfDoors);
+
+    }
+    static class Car {
+        public int numberOfDoors = 4;
+        public void incrementDoors() {
+            this.numberOfDoors++;
+        }
+    }
+}
